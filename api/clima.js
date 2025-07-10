e998ed677b464b9c91e3bfae5f7e6e793d65c746
Javi Gelberg <128005377+jfgelberg@users.noreply.github.com>
2025-07-10 08:16:34 -0300

    Update clima.js
    update 9

--- api/clima.js
+++ api/clima.js
@@ -1,4 +1,3 @@
-regionactiva-api/api/clima.js
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*'); // permitir todos los orígenes (o usar https://regionactiva.com)
   res.setHeader('Access-Control-Allow-Methods', 'GET');
